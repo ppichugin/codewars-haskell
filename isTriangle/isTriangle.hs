68e7ac72e1aab74df8aca5e854a10b5f4a38d0fc
Petr Pichugin <petr.pichugin@gmail.com>
2022-12-28 11:11:04 +0500

    isTriangle solutuion done

---
+++ isTriangle/isTriangle.hs
@@ -0,0 +1,25 @@
+module Codewars.Triangles where
+
+import Data.List (sort)
+
+-- simple solution
+isTriangle :: Int -> Int -> Int -> Bool
+isTriangle a b c
+  | (a + b <= c) || (a + c <= b) || (b + c <= a) = False
+  | (a <= 0) || (b <= 0) || (c <= 0) = False
+  | otherwise = True
+
+-- with sort function
+-- isTriangle :: Int -> Int -> Int -> Bool
+-- isTriangle a b c =
+--   case sort [a,b,c] of
+--      [min, middle, max] -> (min + middle) > max
+
+-- nice solution
+-- isTriangle :: Int -> Int -> Int -> Bool
+-- isTriangle a b c = (a + b > c) && (b + c > a) && (c + a > b)
+
+-- one more nice solution
+-- isTriangle :: Int -> Int -> Int -> Bool
+-- isTriangle a b c = x + y > z
+--   where [x, y, z] = sort [a, b, c]
