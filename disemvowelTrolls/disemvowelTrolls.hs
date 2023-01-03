4aa80caae0c3ffac2c821ed027f32adc14ba2203
Petr Pichugin <petr.pichugin@gmail.com>
2023-01-03 18:33:26 +0500

    kata 'Disemvowel Trolls' solved

---
+++ disemvowelTrolls/disemvowelTrolls.hs
@@ -0,0 +1,10 @@
+module Disemvowel where
+
+import Data.Char
+
+isVowel :: Char -> Bool
+isVowel x = y == 'a' || y == 'e' || y == 'i' || y == 'o' || y == 'u' 
+ where y = toLower x
+
+disemvowel :: String -> String
+disemvowel str = [ch | ch <- str, not (isVowel ch)]
